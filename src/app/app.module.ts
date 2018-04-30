4ce53a3e930176ce6bc5804883410a6e44eac36b
jighead <gerdsuhr@gmail.com>
2018-04-30 09:44:53 -0400

    Added addItem and removeItem methods and animation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/* we need this module for aninamtions, first install it via npm.
+  npm  install @angular/animations@latest --save
+*/
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
+/* we need this module for our form */
+import { FormsModule } from '@angular/forms';
+// the default main component
 import { AppComponent } from './app.component';
+// our home component
 import { HomeComponent } from './home/home.component';
+// our about component
 import { AboutComponent } from './about/about.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
+    HomeComponent, // added out home componenent
+    AboutComponent // added our about componenent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // added forms module
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
