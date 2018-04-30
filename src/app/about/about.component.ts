69876088ae2b00f4facb5edc76c7b70101eeb25a
jighead <gerdsuhr@gmail.com>
2018-04-30 13:15:51 -0400

    Added a data service and updated about page to use it.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router'; // we need this to use route parameters.
 import {Router} from '@angular/router';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -9,16 +9,20 @@ import {Router} from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-    // dependency inject ActivateRoute and Router module into constructor.
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
+    // dependency inject ActivateRoute, Router and Service module into constructor.
     // ActivatedRoute module is used fo main navigation
     // Router module is used for the goToHomePage method below
+     // Dataservice module is used for sharing the goals  array across out 2 components.
     this.route.params.subscribe(
       res => console.log(res.id) // reads the url parameter "id".
     );
   }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res); // load our goals
   }
 
   goToHomePage() {
